56c717ff559827441d5da36be6578bca588c6d06
bricebou <brice@reclic.dev>
2023-01-24 20:49:31 +0100

    Mise en place d'un controller minimal de base pour la page d'accueil

---
+++ src/Controller/SortieController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SortieController extends AbstractController
+{
+    #[Route('/', name: 'sortie_index')]
+    public function index(): Response
+    {
+        return $this->render('sortie/index.html.twig', [
+            'controller_name' => 'SortieController',
+        ]);
+    }
+}
