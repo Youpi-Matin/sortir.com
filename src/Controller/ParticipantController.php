b7c7bb92d089d2c89e5dd0fafce8305075a11a82
bricebou <brice@reclic.dev>
2023-01-25 11:05:18 +0100

    Fix UserPasswordHasherInterface

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -4,46 +4,49 @@ namespace App\Controller;
 
 use App\Entity\Participant;
 use Doctrine\ORM\EntityManagerInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use App\Form\ParticipantType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ParticipantController extends AbstractController
 {
     #[Route('/profil/edit/{id}', name: 'participant_edit')]
-    public function edit(Participant $participant, Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $hashedPassword): Response
-    {
-		$formulaireParticipant = $this->createForm(ParticipantType::class, $participant);
-		
-		$formulaireParticipant->handleRequest($request);
+    public function edit(
+        Participant $participant,
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $hasher
+    ): Response {
+        $formulaireParticipant = $this->createForm(ParticipantType::class, $participant);
 
-		if ($formulaireParticipant->isSubmitted() && $formulaireParticipant->isValid())
-		{
-			$hashedPassword = $passwordHasher->hashPassword(
-				$participant,
-				$participant->getPassword()
-			);
-			$participant->setPassword($hashedPassword);
-			
+        $formulaireParticipant->handleRequest($request);
 
-			$entityManager->persist($participant);
-			$entityManager->flush();
+        if ($formulaireParticipant->isSubmitted() && $formulaireParticipant->isValid()) {
+            $hashedPassword = $hasher->hashPassword(
+                $participant,
+                $participant->getPassword()
+            );
+            $participant->setPassword($hashedPassword);
 
-			$this->addFlash('success', "Profil modifié !");
-		}
+
+            $entityManager->persist($participant);
+            $entityManager->flush();
+
+            $this->addFlash('success', "Profil modifié !");
+        }
         return $this->render('participant/edit.html.twig', [
-			'formulaireParticipant' => $formulaireParticipant->createView()
+            'formulaireParticipant' => $formulaireParticipant->createView()
         ]);
     }
 
-	#[Route('/profil/{id}', name: 'participant_view')]
+    #[Route('/profil/{id}', name: 'participant_view')]
     public function view(Participant $participant): Response
     {
-		return $this->render('participant/view.html.twig', [
-			'participant' => $participant
+        return $this->render('participant/view.html.twig', [
+            'participant' => $participant
         ]);
     }
 }
