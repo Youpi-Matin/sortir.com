5686198c37d26da308b8042cb6b1cfb3c7c80e27
Patrice <dev@allary-guillery.fr>
2023-01-26 13:21:11 +0100

    Implémentation de l'annulation de la sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Etat;
 use App\Entity\Participant;
 use App\Entity\Sortie;
+use App\Form\SortieCancelType;
 use App\Model\SortieFiltre;
 use App\Form\SortieCreationType;
 use App\Form\SortieFiltreType;
@@ -57,6 +58,7 @@ class SortieController extends AbstractController
     #[Route('/sortie/create', name: 'sortie_create', methods: ['GET', 'POST'])]
     public function create(Request $request, ManagerRegistry $doctrine): Response
     {
+
         // Interdit l'acces si non authentifié
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         // Récupère l'organisteur
@@ -69,7 +71,7 @@ class SortieController extends AbstractController
         $sortie->setCampus($user->getCampus());
         $sortie->setDateHeureDebut(new \DateTime('tomorrow'));
         $sortie->setDateLimiteInscription(new \DateTime('now'));
-        $sortie->setEtat($doctrine->getRepository(Etat::class)->findOneBy(['libelle' => 'Créée']));
+        //$sortie->setEtat($doctrine->getRepository(Etat::class)->findOneBy(['libelle' => 'Créée']));
 
         $form = $this->createForm(SortieCreationType::class, $sortie);
 
@@ -89,9 +91,39 @@ class SortieController extends AbstractController
 
         return $this->render('sortie/create.html.twig', [
             'form' => $form,
-            'etat' => $sortie->getEtat()->getLibelle(),
+            'etat' => 'Null',
             'sortie_id' => $sortie->getId(),
         ]);
+    }
+
+    #[Route('sortie/publish/{id}', name: 'sortie_publish')]
+    public function publish(int $id, Request $request, ManagerRegistry $doctrine): Response
+    {
+        // Interdit l'acces si non authentifié
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        // Récupère l'orginisateur de la sortie
+        $sortie = $doctrine->getRepository(Sortie::class)->findOneBy(['id' => $id]);
+
+        if ($id === 0) {
+            dd($request->getRequestUri());
+        }
+
+
+        // Si l'utilisateur n'est pas l'organisateur -> Acccess Denied
+        if ($sortie->getOrganisateur() !== $this->getUser()) {
+            throw $this->createAccessDeniedException('Impossible d\'acceder à cette page !');
+        }
+
+        $sortie = $doctrine->getRepository(Sortie::class)->find($id);
+        $sortie->setEtat($doctrine->getRepository(Etat::class)->findOneBy(['libelle' => 'Ouverte']));
+        $manager = $doctrine->getManager();
+        $manager->persist($sortie);
+        $manager->flush();
+
+        $this->addFlash('success', 'Sortie Ouverte aux inscriptions avec succès');
+
+        return $this->redirectToRoute('sortie_index');
     }
 
     #[Route('/sortie/manage/{id}', name: 'sortie_manage', methods: ['GET', 'POST'])]
@@ -131,8 +163,17 @@ class SortieController extends AbstractController
         ]);
     }
 
-    #[Route('sortie/cancel/{id}', name: 'sortie_cancel')]
-    public function cancel(int $id, ManagerRegistry $doctrine): Response
+    /**
+     * Annulation d'une sortie
+     * Utilise un formaulaire contenant seulement le motif de l'annulation
+     * Le motif remplacera les infos de la sortie.
+     * @param int $id L'id de la sortie à annuler
+     * @param Request $request
+     * @param ManagerRegistry $doctrine
+     * @return Response
+     */
+    #[Route('sortie/cancel/{id}', name: 'sortie_cancel', methods: ['POST', 'GET'])]
+    public function cancel(int $id, Request $request, ManagerRegistry $doctrine): Response
     {
         // Interdit l'acces si non authentifié
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
@@ -146,38 +187,25 @@ class SortieController extends AbstractController
         }
 
         $sortie = $doctrine->getRepository(Sortie::class)->find($id);
-        $sortie->setEtat($doctrine->getRepository(Etat::class)->findOneBy(['libelle' => 'Annulée']));
-        $manager = $doctrine->getManager();
-        $manager->persist($sortie);
-        $manager->flush();
+        $form = $this->createForm(SortieCancelType::class, $sortie);
+        $form->handleRequest($request);
 
-        $this->addFlash('success', 'Sortie Annulée avec succès');
-
-        return $this->redirectToRoute('sortie_index');
-    }
-
-    #[Route('sortie/publish/{id}', name: 'sortie_publish')]
-    public function publish(int $id, ManagerRegistry $doctrine): Response
-    {
-        // Interdit l'acces si non authentifié
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-
-        // Récupère l'orginisateur de la sortie
-        $sortie = $doctrine->getRepository(Sortie::class)->findOneBy(['id' => $id]);
-
-        // Si l'utilisateur n'est pas l'organisateur -> Acccess Denied
-        if ($sortie->getOrganisateur() !== $this->getUser()) {
-            throw $this->createAccessDeniedException('Impossible d\'acceder à cette page !');
+        if ($form->isSubmitted() && $form->isValid()) {
+            // On remplace l'état de la sortie par 'Annulée'
+            $sortie->setEtat($doctrine->getRepository(Etat::class)->findOneBy(['libelle' => 'Annulée']));
+            // On remplace la description par le motif de l'annulation
+            $sortie->setInfosSortie($form->get('motif')->getData());
+            $manager = $doctrine->getManager();
+            $manager->persist($sortie);
+            $manager->flush();
+            $this->addFlash('success', 'Sortie Annulée avec succès');
+            return $this->redirectToRoute('sortie_index');
         }
 
-        $sortie = $doctrine->getRepository(Sortie::class)->find($id);
-        $sortie->setEtat($doctrine->getRepository(Etat::class)->findOneBy(['libelle' => 'Ouverte']));
-        $manager = $doctrine->getManager();
-        $manager->persist($sortie);
-        $manager->flush();
 
-        $this->addFlash('success', 'Sortie Ouverte aux inscriptions avec succès');
-
-        return $this->redirectToRoute('sortie_index');
+        return $this->render('sortie/cancel.html.twig', [
+            'form' => $form,
+            'sortie' => $sortie,
+        ]);
     }
 }
