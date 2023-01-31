05e7f3e1e6026c2e13fbc9078dbe24d61530ee29
Patrice <dev@allary-guillery.fr>
2023-01-31 23:51:25 +0100

    implémentation méthodes subscribe / unsubscribe sur voter

--- src/Security/AppVoter.php
+++ src/Security/AppVoter.php
@@ -12,6 +12,8 @@ class AppVoter extends Voter
     public const VIEW = 'view';
     public const EDIT = 'edit';
     public const ADMIN = 'admin';
+    public const SUBSCRIBE = 'subscribe';
+    public const UNSUBSCRIBE = 'unsubscribe';
 
 
     /**
@@ -20,7 +22,7 @@ class AppVoter extends Voter
     protected function supports(string $attribute, mixed $subject): bool
     {
         // if the attribute isn't one we support, return false
-        if (!in_array($attribute, [self::VIEW, self::EDIT, self::ADMIN])) {
+        if (!in_array($attribute, [self::VIEW, self::EDIT, self::ADMIN, self::SUBSCRIBE, self::UNSUBSCRIBE])) {
             return false;
         }
 
@@ -43,20 +45,40 @@ class AppVoter extends Voter
             self::VIEW => $this->canView($subject, $user),
             self::EDIT => $this->canEdit($subject, $user),
             self::ADMIN => $this->isAdmin($user),
+            self::SUBSCRIBE => $this->canSubscribe($subject, $user),
+            self::UNSUBSCRIBE => $this->canUnsubscribe($subject, $user),
             default => throw new \LogicException('This code should not be reached!')
         };
     }
 
+    /** Check utilisateur connecté (autorisation par défaut)
+     * @param mixed $subject
+     * @param Participant $user
+     * @return bool
+     */
     private function canView(mixed $subject, Participant $user): bool
     {
         // Si on peut editer on peut voir
         if ($this->canEdit($subject, $user)) {
             return true;
         }
+        // Si on peut s'inscrire on peut voir
+        if ($this->canSubscribe($subject, $user)) {
+            return true;
+        }
+        // Si on peut se desinsdcrire, on peut voir
+        if ($this->canUnsubscribe($subject, $user)) {
+            return true;
+        }
 
         return true;
     }
 
+    /** Check utilisateur autorisé à modifier les informations
+     * @param mixed $subject
+     * @param Participant $user
+     * @return bool
+     */
     private function canEdit(mixed $subject, Participant $user): bool
     {
         if ($this->isAdmin($user)) {
@@ -74,8 +96,52 @@ class AppVoter extends Voter
         return false;
     }
 
+    /** Check user isAdmin
+     * @param Participant $user
+     * @return bool|null
+     */
     private function isAdmin(Participant $user)
     {
         return $user->isAdministrateur();
+    }
+
+    /** Check si l'utilisateur peut s'inscrire à la sortie
+     * Il faut que la sortie soit ouverte et le nombre d'inscrit < nombre de place
+     * Et que le participant ne soit pas deja inscrit.
+     * @param Sortie $sortie
+     * @param Participant $user
+     * @return void
+     */
+    private function canSubscribe(Sortie $sortie, Participant $user): bool
+    {
+        if (
+            count($sortie->getParticipants()) < $sortie->getNbInscriptionsMax()
+            && $sortie->getEtat()->getLibelle() === 'Ouverte'
+            && !$sortie->getParticipants()->contains($user)
+        ) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Check si l'utilisateur peut se desinscrire à la sortie
+     * Il faut que la sortie soit ouverte ou cloturée
+     * et la date du jour < date limite
+     * et utilisateur inscrit
+     *
+     * @param Sortie $sortie
+     * @param Participant $user
+     * @return void
+     */
+    private function canUnsubscribe(Sortie $sortie, Participant $user): bool
+    {
+        if (
+            ($sortie->getEtat()->getLibelle() === 'Ouverte' || $sortie->getEtat()->getLibelle() === 'Clôturée')
+            && $sortie->getDateHeureDebut() > new \DateTime('now')
+            && $sortie->getParticipants()->contains($user)
+        ) {
+            return true;
+        }
+        return false;
     }
 }
