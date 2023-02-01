e2ea110a051ea04fcdae65b537e4aca110215540
Patrice <dev@allary-guillery.fr>
2023-02-01 17:43:26 +0100

    renommage commandes

--- src/Service/ActiveSortieCommand.php
+++ src/Service/ActiveSortieCommand.php
@@ -9,7 +9,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
-    name: 'app:active-sortie',
+    name: 'app:sortie:activer',
     description: 'Active les sorties dont la date et l\'heure sont actuelles.'
 )]
 class ActiveSortieCommand extends Command
